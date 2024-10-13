e70a0e70d6eb957ad07b9c21728c7cbe3ad49ce9
Rafał Buchner <rafal.buchner@gmail.com>
2024-10-13 13:56:17 +0200

    update

--- lib/GlyphsApp/__init__.py
+++ lib/GlyphsApp/__init__.py
@@ -1,5 +1,9 @@
 from GlyphsApp.GSFont import GSFont
 from GlyphsApp.GSApplication import GSApplication
+from GlyphsApp.GSLayer import GSLayer
+
 
 Glyphs = GSApplication()
 Glyphs.font = GSFont()
+
+__all__ = ["GSFont", "GSApplication", "GSLayer", "Glyphs"]
